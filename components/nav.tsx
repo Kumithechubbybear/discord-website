4399774a7912fa683b0420d0b3ae1646a682375e
ronnapatp <ronnapatp@users.noreply.github.com>
2021-09-30 12:16:57 +0700

    Update nav , create other page

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,17 +1,16 @@
 import Link from "next/link";
-import Image from 'next/image';
 
 
 export default function Nav() {
   return (
     <nav className="container-fluid">
     <ul>
-      <li><a href="/" className="contrast"><strong>Kumi discord sever</strong></a></li>
+      <li className="contrast"><Link href='/'><a><strong>Kumi discord sever</strong></a></Link></li>
     </ul>
     <ul>
-      <li><a href="#" className="contrast" >Home</a></li>
-      <li><a href="#" className="contrast" >Server info</a></li>
-      <li><a href="#" className="contrast" >Contact us</a></li>
+      <li className="contrast"><Link href='/'><a>Home</a></Link></li>
+      <li className="contrast"><Link href='/sever'><a>Server info</a></Link></li>
+      <li className="contrast"><Link href='/contact'><a>Contact us</a></Link></li>
     </ul>
   </nav>
   );
