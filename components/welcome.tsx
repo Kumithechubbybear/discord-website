f8e4ba6d264ab2c64a036ea1123e1d208d51c270
ronnapatp <ronnapatp@users.noreply.github.com>
2021-09-30 13:35:02 +0700

    Update welcomre

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -1,30 +1,26 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from "next/link";
 
 
 export default function Welcome() {
     return (
         <div className="container">
             <div className="center-text">
-                <h1>Kumi server</h1>
-            </div>
-        <article className="grid">
-          <div>
-            <hgroup>
-              <h1>Sign in</h1>
-              <h2>A minimalist layout for Login pages</h2>
-            </hgroup>
-            <form>
-              <fieldset>
-                  Remember me
-              </fieldset>
-              <button type="submit" className="contrast">Login</button>
-            </form>
-          </div>
-          <div>
-            <Image src="/kumiprofile.jpg" width={100} height={100} />
+                <br />
+                <h1>Kumi discord server 🐻</h1>
+                <h3>Join <Link href="https://discord.gg/etYUej9Gzw"><a target="_blank">Kumi discord server</a></Link></h3>
+                <Image src="/kumiprofile.jpg" width={250} height={250} />
 
-          </div>
-        </article>
+            </div>
+            <br />
+            <section id="preview">
+        <h2>Our server</h2>
+        <p>We have a lot of thing you that make you want to join! <Link href="https://discord.gg/etYUej9Gzw"><a target="_blank">CLick here to join</a></Link></p>
+        <form>
+
+          <button type="submit">Subscribe</button>
+        </form>
+      </section>
       </div>
     );
   }
