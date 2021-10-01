b3bbd442f747ddd4d1afa63e1d8c9f6b2c3122ca
ronnapatp <ronnapatp@users.noreply.github.com>
2021-10-01 11:22:29 +0700

    Update faq

--- components/question.tsx
+++ components/question.tsx
@@ -1,4 +1,4 @@
-//import Link from "next/link";
+import Link from "next/link";
 
 import Question from "./faq";
 
@@ -9,14 +9,25 @@ export default function Fqa() {
         <h2>FAQ</h2>
         <Question
             question="How do I get unban"
-            answer="You can get unban by fill in ban appeal. Go to banappeal page to get form link" />
+            answer="" />
+            <p className="">
+            You can get unban by fill in ban appeal. Go to <Link href='#'><a>banappeal page</a></Link> to get form link
+            </p>
 
         <Question
             question="How to become a moderator in kumi server"
             answer="" />
             <p className="">
-              If we need more moderator we will post in <kbd>#announcement</kbd> channel
+              If we need more moderator we will post in <Link href='https://discord.com/channels/884365961543684126/884402109821374514'><a target="_blank"><kbd>#announcement</kbd></a></Link> channel
             </p>
+
+        <Question
+            question="How to contribute in kumi website"
+            answer="" />
+            <p className="">
+              Open <Link href='https://github.com/Kumithechubbybear/New-KumiWeb/pulls'><a target="_blank">github pullrequest</a></Link> and open pulls also add description about what you contribute
+            </p>
+
 
         </div>
   );
